b9cc91f6f6093b056ab026269a789020c36502fd
blasscoc <blasscoc@gmail.com>
2024-10-09 00:52:10 +0000

    fix: remove kruft

--- .devcontainer/post-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Ensure that Poetry environment is activated
-echo "Activating Poetry environment..."
-source $(poetry env info --path)/bin/activate
-
-# Install any additional Python packages if needed
-echo "Installing additional Python packages..."
-poetry install
